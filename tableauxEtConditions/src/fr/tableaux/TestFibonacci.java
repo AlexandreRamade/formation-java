2089589860847dd6d961b56bc8117fda3346366e
AlexandreR <alexandre.ramade@sfr.fr>
2019-05-03 12:12:36 +0200

    ajout projet pizerria

--- tableauxEtConditions/src/fr/tableaux/TestFibonacci.java
+++ tableauxEtConditions/src/fr/tableaux/TestFibonacci.java
@@ -3,15 +3,17 @@ package fr.tableaux;
 import java.util.Scanner;
 
 public class TestFibonacci {
-
+	
+	//ATTENTION : valable seulement jusqu'au rang 92 à cause de la limitation des nombres long
+	
 	public static void main(String[] args) {
 		System.out.println("*****   Suite de Fibonacci   *****");
 		System.out.println("\nEntrez le rang N du nombre que vous souhaitez connaitre dans la suite :");
 		Scanner questionUser = new Scanner(System.in);
 		int rangN = questionUser.nextInt();
 		
-		int valeurNmoins1 = 1;
-		int valeurN = 1;
+		long valeurNmoins1 = 1L;
+		long valeurN = 1L;
 		
 		if(rangN == 0) {
 			valeurN = 0;
@@ -21,7 +23,8 @@ public class TestFibonacci {
 				valeurNmoins1 = valeurN - valeurNmoins1;
 			}
 		}		
-		System.out.print("Dans la suite de fibonacci, le nombre de rang " + rangN + " vaut : " + valeurN);
+		System.out.println("Dans la suite de Fibonacci, le nombre de rang " + rangN + " vaut : " + valeurN);
+		System.out.println("Le quotient N/N-1 vaut : " + (float) valeurN/valeurNmoins1);
 	}
 
 }
